ef012444000d6ecaa44784c7752dd01591af8630
DragonKnightOfBreeze <dk_breeze@qq.com>
2022-12-15 03:18:01 +0800

    0.2.2 更新中

--- src/main/kotlin/icu/windea/breeze/toolbox/codeInsight/hints/I18nMessageHintsProviderFactory.kt
+++ src/main/kotlin/icu/windea/breeze/toolbox/codeInsight/hints/I18nMessageHintsProviderFactory.kt
@@ -6,16 +6,18 @@ import com.intellij.uast.UastMetaLanguage
 
 @Suppress("UnstableApiUsage")
 class I18nMessageHintsProviderFactory: InlayHintsProviderFactory {
+	private val uastMetaLanguage = Language.findInstance(UastMetaLanguage::class.java)
+	
 	override fun getProvidersInfo(): List<ProviderInfo<out Any>> {
-		return I18nMessageHintsProvider().let { p -> getLanguages().map { l -> ProviderInfo(l, p) } }
+		return I18nMessageHintsProvider().let { p -> uastMetaLanguage.matchingLanguages.map { l -> ProviderInfo(l, p) } }
 	}
 
 	override fun getProvidersInfoForLanguage(language: Language): List<InlayHintsProvider<out Any>> {
-		if(UastMetaLanguage.getRegisteredLanguages().none { it == language }) return emptyList()
+		if(uastMetaLanguage.matchesLanguage(language)) return emptyList()
 		return I18nMessageHintsProvider().let { listOf(it) }
 	}
 	
 	override fun getLanguages(): Iterable<Language> {
-		return UastMetaLanguage.getRegisteredLanguages()
+		return uastMetaLanguage.matchingLanguages
 	}
 }
